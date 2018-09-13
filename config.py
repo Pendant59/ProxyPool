18f0419863e02ee1df99f5dad65fd59500d83267
Pandent <hjsjue@123.com>
2018-09-13 19:13:10 +0800

    新增header配置

--- config.py
+++ config.py
@@ -8,9 +8,19 @@ PASSWORD = ''
 # 数据库 0-15
 DB = 0
 
-
 # 获取代理的模式 0：取出即删除，1：仅取出
 GET_PROXY_TYPE = 0
+
+# 获取代理的header
+HEADERS_LIST = [
+	'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 UBrowser/4.0.3214.0 Safari/537.36',
+	'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36',
+	'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0',
+	'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
+	'Mozilla/5.0 (WindowsNT6.1;rv:2.0.1)Gecko/20100101Firefox/4.0.1',
+	'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36',
+]
+
 
 # 获得代理测试时间界限
 get_proxy_timeout = 9
