f3c23b9b8a5860d9c225a986869eea0d756402dd
Pandent <hjsjue@123.com>
2018-09-18 14:58:43 +0800

    修改配置项

--- config.py
+++ config.py
@@ -23,15 +23,15 @@ HEADERS_LIST = [
 
 
 # 获得代理测试时间界限
-get_proxy_timeout = 9
+GET_PROXY_TIMEOUT = 9
 
 # 代理池数量界限
-POOL_LOWER_THRESHOLD = 20
-POOL_UPPER_THRESHOLD = 100
+POOL_MIN_NUMBER = 20
+POOL_MAX_NUMBER = 100
 
 # 检查周期
-VALID_CHECK_CYCLE = 60
-POOL_LEN_CHECK_CYCLE = 20
+VALID_PROXY_CYCLE = 60
+POOL_MAX_LEN_CYCLE = 20
 
 # 测试API，用百度来测试
 TEST_API='https://www.baidu.com'
