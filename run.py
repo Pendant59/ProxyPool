6ec568ad2c2cdb6053f037a284dea0c8a33dec22
Pandent <hjsjue@123.com>
2018-09-20 17:10:51 +0800

    项目启动文件

--- run.py
+++ run.py
@@ -1,2 +1,18 @@
-import main
+from main import Main
+from multiprocessing import Process
+from api import app
+ 
+def run():
+    get_process = Process(target=Main.GrabProxies)
+    check_process = Process(target=Main.CheckProxies)
+    get_process.start()
+    check_process.start()
 
+def api():
+	app.run()
+
+if __name__ == '__main__':
+	# 抓取和检测
+	run()
+	# 提供proxy的接口
+	api()
